55566d7d6bbb0c5c6cd18490e7c89de73f5a874b
Michał Wróbel <michalwrobel@diligentbits.com>
2023-03-30 14:38:42 +0200

    Pass request further to github workflows

--- server.mjs
+++ server.mjs
@@ -1,6 +1,36 @@
-import { createServer } from 'http';
+// Import dependencies
+import express from 'express';
+import axios from 'axios';
 
-createServer((req, res) => {
-  res.write('Hello World!');
-  res.end();
-}).listen(process.env.PORT);
+// Create an Express app
+const app = express();
+app.use(express.json());
+
+// Define the /deploy endpoint
+app.post('/deploy', async (req, res) => {
+  // Rewrite the request body
+  const data = { "event_type": `Deployed due to strapi event webhook: ${new Date().toUTCString()}` };
+  
+  // Send the data to the GitHub API
+  try {
+    const response = await axios.post('https://api.github.com/repos/MichalWrobel2/mp-client/dispatches', data, {
+      headers: {
+        'Authorization': `Bearer ${process.env.GITHUB_TOKEN}`,
+        'Accept': 'application/vnd.github.v3+json'
+      }
+    });
+    
+    // Log the response and send a success message
+    console.log(response.data);
+    res.status(200).json({ message: 'Event dispatched successfully' });
+  } catch (error) {
+    // Log the error and send an error message
+    console.error(error);
+    res.status(500).json({ message: 'Error dispatching event' });
+  }
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});

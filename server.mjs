a6b37811f0434b7a63f9a1c0daa7e4473f55f59c
Michał Wróbel <michalwrobel@diligentbits.com>
2023-03-30 15:20:04 +0200

    Use railway port instead of hard-coded one

--- server.mjs
+++ server.mjs
@@ -31,6 +31,6 @@ app.post('/deploy', async (req, res) => {
 });
 
 // Start the server
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+app.listen(process.env.PORT, () => {
+  console.log(`Server started on port ${process.env.PORT}`);
 });
